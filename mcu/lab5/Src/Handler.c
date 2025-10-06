78d0f63200e7a83e765d80acd6493cc6113c2ebe
Swayngee <drgonzales@g.hmc.edu>
2025-10-06 10:06:55 -0700

    Update Handler.c

--- mcu/lab5/Src/Handler.c
+++ mcu/lab5/Src/Handler.c
@@ -1,12 +1,15 @@
-
+//Drake Gonzales
+//drgonzales@g.hmc.edu
+//This Module is my c file for the interupt handlers
+//1/4/25
 
 #include <stm32l432xx.h>
 #include "INITMAIN.h"
 
 
-volatile int32_t countA = 0;
-volatile int32_t countB = 0;
+volatile int32_t position = 0;
 //below is the code for setting the counter
+
 void EXTI1_IRQHandler(void){
   if(EXTI->PR1 &(1<<1)){
       // If so, clear the interrupt (NB: Write 1 to reset.)
@@ -14,11 +17,11 @@ void EXTI1_IRQHandler(void){
         int A = (GPIOA->IDR & (1 << 1)) != 0;
         int B = (GPIOA->IDR & (1 << 2)) != 0;
 
-        if (A == B){ //A is on when b is falling
-        countA = countA + 1;
+        if (A == B){ //A is on when b is on
+        position = position + 1;
         }
         else{
-        countA = countA - 1 ;
+        position = position - 1 ;
         }
 
     }
@@ -31,14 +34,13 @@ void EXTI2_IRQHandler(void){
 
         int A = (GPIOA->IDR & (1 << 1)) != 0;
         int B = (GPIOA->IDR & (1 << 2)) != 0;
-        if (A != B){ // they are both off on A's fall
-        countB = countB + 1;
+        if (A != B){  // they are both off on A's fall
+        position = position + 1;
         }
         else{
-        countB = countB - 1;
+        position = position - 1;
         }
 
     }
 }
-
 
