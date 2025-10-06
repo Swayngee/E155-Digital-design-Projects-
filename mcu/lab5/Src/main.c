d290b99ec5ab8d41ce4e3109c774c9169216904a
Swayngee <drgonzales@g.hmc.edu>
2025-10-06 10:06:29 -0700

    Update main.c

--- mcu/lab5/Src/main.c
+++ mcu/lab5/Src/main.c
@@ -1,11 +1,16 @@
-
+//Drake Gonzales
+//drgonzales@g.hmc.edu
+//This Module is my main file for lab5
+//1/4/25
+    
 
 #include "INITMAIN.h"
 #include "STM32L432KC.h"
 
 volatile uint32_t rev = 0;
 int dir = 0;
-
+double revolutions =0;
+double zero =0;
 
 int main(void){
 GPIOenable();
@@ -13,23 +18,33 @@ inintAB();
 
 while(1){
 
-if (countA < 0) dir = 1;  
-else if (countB < 0) dir = -1;  
+if (position > 0) {
+    dir = 1;
+} else if (position < 0) {
+    dir = -1;
+}
 
-if (countA  >= 7 || countB >= 7){
-rev = rev + 1;
-countA = 0;
-countB = 0;
+if (position  != 0){
+revolutions += position /7.0;
+
+  if(position >= 7){
+    position = 0;
+  }
 }
 
 delay_millis(TIM7, 1000); 
 if (dir == 1) {
-    printf("Speed = %u rev/s, Direction = CW\n", rev);
+    printf("Speed = %.2f rev/s, Direction = CW\n", revolutions);
 } else if (dir == -1) {
-    printf("Speed = %u rev/s, Direction = CCW\n", rev);
+    printf("Speed = %.2f rev/s, Direction = CCW\n", revolutions);
 }
-rev = 0;
+else {
+    printf("Speed = %.2f rev/s, Direction = unknown\n", zero);
+}
+
+revolutions = 0;
 
 }
 }
+
 
