380fb9071eb0fb3b167b512235c062fe108c40ac
Swayngee <drgonzales@g.hmc.edu>
2025-10-07 21:58:15 -0700

    Update main.c

--- mcu/lab5/Src/main.c
+++ mcu/lab5/Src/main.c
@@ -2,41 +2,43 @@
 //drgonzales@g.hmc.edu
 //This Module is my main file for lab5
 //1/4/25
-    
 
 #include "INITMAIN.h"
 #include "STM32L432KC.h"
 
 volatile uint32_t rev = 0;
 int dir = 0;
-double zero =0;
-
+float zero =0;
+int32_t prev = 0;
+float revolutions =0;
+int32_t d =0;
+int32_t timer = 500;
 int main(void){
-GPIOenable();
+GPIOenable(); //init funcs
+timenable();
 inintAB();
 
 while(1){
-int32_t prev = 0;
-double revolutions =0;
-int32_t d = position - prev;
-prev = position;
-revolutions = (double)d / 1632.0;  
+// In main loop
+d = position;
+position = 0;
+revolutions = (float) (d *(1000/timer)) / (4 * 408);  //408 PPR
 
-if (d > 0) dir = 1;
+if (d > 0) dir = 1; //set directions
 else if (d < 0) dir = -1;
 else dir = 0;
 
-delay_millis(TIM7, 1000); 
+delay_millis(TIM2, timer); //1Hz delay
+
 if (dir == 1) {
-    printf("Speed = %.2f rev/s, Direction = CW\n", revolutions);
+    printf("Speed = %f rev/s, Direction = CW\n", revolutions);
 } else if (dir == -1) {
-    printf("Speed = %.2f rev/s, Direction = CCW\n", revolutions);
+    printf("Speed = %f rev/s, Direction = CCW\n", -revolutions);
 }
+
 else {
-    printf("Speed = %.2f rev/s, Direction = unknown\n", zero);
+printf("Speed = %f rev/s, Direction = none\n", zero);
 }
-
-
 }
 }
 
