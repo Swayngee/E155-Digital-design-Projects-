d235381f03645cd44889a2488456adceab6844f3
Swayngee <drgonzales@g.hmc.edu>
2025-10-07 21:59:07 -0700

    Update Handler.c

--- mcu/lab5/Src/Handler.c
+++ mcu/lab5/Src/Handler.c
@@ -4,6 +4,7 @@
 //1/4/25
 
 #include <stm32l432xx.h>
+#include "STM32L432KC.h"
 #include "INITMAIN.h"
 
 
@@ -14,14 +15,16 @@ void EXTI1_IRQHandler(void){
   if(EXTI->PR1 &(1<<1)){
       // If so, clear the interrupt (NB: Write 1 to reset.)
       EXTI -> PR1 |=(1<<1);
-        int A = (GPIOA->IDR & (1 << 1)) != 0;
-        int B = (GPIOA->IDR & (1 << 2)) != 0;
+
+        int A = (GPIOA->IDR & (1 << gpioPinOffset(pinA))) != 0;
+        int B = (GPIOA->IDR & (1 << gpioPinOffset(pinB))) != 0;
 
         if (A == B){ //A is on when b is on
-        position = position + 1;
+        position = position -1 ; 
         }
+
         else{
-        position = position - 1 ;
+        position = position + 1 ;
         }
 
     }
@@ -32,15 +35,17 @@ void EXTI2_IRQHandler(void){
         // If so, clear the interrupt (NB: Write 1 to reset.)
         EXTI->PR1 |= (1 << 2);
 
-        int A = (GPIOA->IDR & (1 << 1)) != 0;
-        int B = (GPIOA->IDR & (1 << 2)) != 0;
+        int A = (GPIOA->IDR & (1 << gpioPinOffset(pinA))) != 0;
+        int B = (GPIOA->IDR & (1 << gpioPinOffset(pinB))) != 0;
+
         if (A != B){  // they are both off on A's fall
-        position = position + 1;
+        position = position - 1; 
         }
         else{
-        position = position - 1;
-        }
+        position = position + 1; 
+        } 
 
     }
 }
+
 
