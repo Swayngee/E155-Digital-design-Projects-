1f56d346e70fa1a1ef6d4d41a7cbe5fd11bad30b
Swayngee <drgonzales@g.hmc.edu>
2025-10-08 15:12:05 -0700

    Update INITMAIN.h

--- mcu/lab5/Src/INITMAIN.h
+++ mcu/lab5/Src/INITMAIN.h
@@ -8,14 +8,14 @@
 #define INITMAIN_H
 
 #include <stm32l432xx.h>
-
+//PA1 and PA2 are 5V acceptable pins. PA2 is p7 on the board 
 #define pinB PA2
 #define pinA PA1
 
 #define DELAY_TIM TIM2
-
+//make position a global var so that the main can use it. Searched this 'extern' func online
 extern volatile int32_t position;
-
+//init funcs
 void inintAB(void);
 void EXTI2_IRQHandler(void);
 void EXTI1_IRQHandler(void);
