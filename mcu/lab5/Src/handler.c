73ca748b806fdd379623b97d2dc3672a2faa7cf7
Swayngee <drgonzales@g.hmc.edu>
2025-10-08 17:00:42 -0700

    Update handler.c

--- mcu/lab5/Src/handler.c
+++ mcu/lab5/Src/handler.c
@@ -10,34 +10,34 @@
 
 volatile int32_t position = 0;
 //below is the code for setting the counter
-// position is the counter value based on each interrupt
+//position is the counter value based on each interrupt
 void EXTI1_IRQHandler(void){
-  if(EXTI->PR1 &(1<<1)){ // Triggers the interrupt. If this condition is true, the following code runs
-      EXTI -> PR1 |=(1<<1); // If so, clear the interrupt (NB: Write 1 to reset.)
+  if(EXTI->PR1 &(1<<1)){ // triggers the interrupt. If this condition is true, the following code runs
+      EXTI -> PR1 |=(1<<1); // if so, clear the interrupt (NB: Write 1 to reset.)
 
         int A = (GPIOA->IDR & (1 << gpioPinOffset(pinA))) != 0; // int A is the encoder A signal from the motor
         int B = (GPIOA->IDR & (1 << gpioPinOffset(pinB))) != 0; // int B is the encoder B signal from the motor
 
         if (A == B){ //A is on when b is on
-        position = position -1 ; // Side effect from interrupt
+        position = position -1 ; // side effect from interrupt
         }
 
         else{
-        position = position + 1 ; // Side effect from interrupt
+        position = position + 1 ; // side effect from interrupt
         }
 
     }
 }
 
 void EXTI2_IRQHandler(void){
-    if (EXTI->PR1 & (1 << 2)){ // This code directly triggers the interrupt. If this condition is true, the following code runs
-        EXTI->PR1 |= (1 << 2); // If so, clear the interrupt (NB: Write 1 to reset.)
+    if (EXTI->PR1 & (1 << 2)){ // this code directly triggers the interrupt. If this condition is true, the following code runs
+        EXTI->PR1 |= (1 << 2); // if so, clear the interrupt (NB: Write 1 to reset.)
 
         int A = (GPIOA->IDR & (1 << gpioPinOffset(pinA))) != 0; // int A is the encoder A signal from the motor
         int B = (GPIOA->IDR & (1 << gpioPinOffset(pinB))) != 0; // int B is the encoder B signal from the motor
 
         if (A != B){  // they are both off on A's fall
-        position = position - 1; // Side effect from interrupt
+        position = position - 1; // side effect from interrupt
         }
         else{
         position = position + 1; // Side effect from interrupt
